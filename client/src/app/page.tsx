7f30e9aa8dde49daa925eddbf81717437945e511
IgnacioOE <A01741599@tec.mx>
2025-03-13 08:49:54 -0600

    trying to connect

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,15 @@
-
+'use client'
+import { useState } from 'react';
 
 export default function Home() {
+  const [result, setResult] = useState<number | null>(null);
+
+  const handleCalculate= async () => {
+    
+    const response = await fetch('http://localhost:3000/result')
+    const data = await response.json();
+    setResult(data.result);
+  }
   return (
     <div className="min-h-screen flex flex-col items-center justify-center gap-8">
       <div className="grid grid-cols-3 gap-4">
@@ -15,7 +24,7 @@ export default function Home() {
         <input type="number" className="border border-gray-300 p-2" placeholder="Inserte un número" />
       </div>
       <button 
-        type="submit"
+        onClick={handleCalculate}
         className="bg-blue-600 rounded-md px-4 py-2 hover:bg-blue-700 transition-colors cursor-pointer"
       >
         Calcular
--- server/index.js
+++ server/index.js
@@ -1,8 +1,33 @@
-import express from 'express';
+const express = require('express');
 const app = express();
 
-app.get("/result", (req1, req2, req3, res) => {
-    res.json({message: "Hello World!"});
+app.get("/result", (req, res) => {
+    const { num1, num2, op } = req.query;
+    if (!num1 || !num2 || !op) {
+        return res.status(400).json({ error: "Faltan parámetros" });
+    }
+    const number1 = parseFloat(num1);
+    const number2 = parseFloat(num2);
+    let result;
+    switch (op) {
+        case "+":
+            result = number1 + number2;
+            break;
+        case "-":
+            result = number1 - number2;
+            break;
+        case "*":
+            result = number1 * number2;
+            break;
+        case "/":
+            result = number1 / number2;
+            break;
+        default:
+            return res.status(400).json({ error: "Operador no válido" });
+    }
+
+    res.json({result});
+    
 });
 
 app.listen(3000, () => {
